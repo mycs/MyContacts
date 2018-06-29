f4c2c983fa24f9f23441d03e38f7d0f1b74d37fa
Michael Y. C. Shih <i420cc@gmail.com>
2018-06-29 10:32:34 +0800

    initial

---
+++ app/src/main/java/cc/i420/mycontacts/MainActivity.java
@@ -0,0 +1,20 @@
+package cc.i420.mycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+        
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
